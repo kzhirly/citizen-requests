e58818a076fde6d380a71cdbc860e0e4266d472d
Nadya <144894071+kzhirly@users.noreply.github.com>
2026-04-27 17:08:16 +0300

    Исправила ошибки

--- фуллстек/citizen-requests/backend/app/core/security.py
+++ фуллстек/citizen-requests/backend/app/core/security.py
@@ -1,4 +1,3 @@
-# app/core/security.py
 from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
@@ -7,9 +6,13 @@ from app.core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
+    # bcrypt ограничение — обрезаем до 72 байт
+    password = password[:72]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # bcrypt ограничение — обрезаем до 72 байт
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict) -> str:
